3adb3690e839995913f3a6c5348fc2e2a5b30b57
rainjke <semkacereza1@gmail.com>
2024-05-22 18:45:02 +0800

    сделал faq

--- webproject/js/faq.js
+++ webproject/js/faq.js
@@ -1,10 +1,27 @@
-function expandContent(contentId, button) {
-  var content = document.getElementById(contentId);
-  if (content.style.display === "block") {
-      content.style.display = "none";
-      button.style.backgroundImage = "url('../img/global/down.svg')";
-  } else {
-      content.style.display = "block";
-      button.style.backgroundImage = "url('../img/global/up.svg')";
-  }
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const buttons = document.querySelectorAll('.expand-button');
+
+  buttons.forEach(button => {
+      button.addEventListener('click', function () {
+          const contentId = this.getAttribute('aria-controls');
+          const content = document.getElementById(contentId);
+          
+          buttons.forEach(btn => {
+              const otherContentId = btn.getAttribute('aria-controls');
+              const otherContent = document.getElementById(otherContentId);
+              if (btn !== this) {
+                  otherContent.style.display = "none";
+                  btn.classList.remove('active');
+              }
+          });
+
+          if (content.style.display === "block") {
+              content.style.display = "none";
+              this.classList.remove('active');
+          } else {
+              content.style.display = "block";
+              this.classList.add('active');
+          }
+      });
+  });
+});
